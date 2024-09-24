734d5eb94e3d866058e7260a0ba2fc2e41b83d52
Gary Sassano <10464497+garysassano@users.noreply.github.com>
2024-09-24 05:50:31 +0200

    update

--- src/functions/http/index.ts
+++ src/functions/http/index.ts
@@ -2,17 +2,23 @@ import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { APIGatewayProxyHandlerV2 } from "aws-lambda";
 
+// Initialize AWS clients
 const s3Client = new S3Client();
-const bucketName = process.env.REKOGNITION_BUCKET_NAME;
+
+// Retrieve and validate the rekognition bucket name from env vars
+const rekognitionBucketName = process.env.REKOGNITION_BUCKET_NAME;
+if (!rekognitionBucketName) {
+  throw new Error("REKOGNITION_BUCKET_NAME env var is required.");
+}
 
 export const handler: APIGatewayProxyHandlerV2 = async (event) => {
   console.log(`Event: ${JSON.stringify(event)}`);
 
-  const body = JSON.parse(event.body || "{}");
+  const body = JSON.parse(event.body ?? "{}");
   const filename = body.filename ?? "unknown";
 
   const command = new PutObjectCommand({
-    Bucket: bucketName,
+    Bucket: rekognitionBucketName,
     Key: filename,
     ChecksumAlgorithm: "SHA256",
   });
